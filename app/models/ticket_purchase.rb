b3200a45db0f4baa9c5c24053a206ede9905108c
AEtherC0r3 <master@aetherc0r3.eu>
2017-04-19 16:47:59 +0300

    Add 'week' column to TicketPurchases

--- app/models/ticket_purchase.rb
+++ app/models/ticket_purchase.rb
@@ -18,6 +18,8 @@ class TicketPurchase < ActiveRecord::Base
   scope :by_conference, ->(conference) { where(conference_id: conference.id) }
   scope :by_user, ->(user) { where(user_id: user.id) }
 
+  after_create :set_week
+
   def self.purchase(conference, user, purchases)
     errors = []
     ActiveRecord::Base.transaction do
@@ -57,5 +59,14 @@ class TicketPurchase < ActiveRecord::Base
 
     purchase.quantity = quantity if quantity > 0
     purchase
+  end
+end
+
+private
+
+def set_week
+  self.week = created_at.strftime('%W')
+  without_versioning do
+    save!
   end
 end
