d286b5b59329367fd4de391426ba1a4448cd4fc3
AEtherC0r3 <master@aetherc0r3.eu>
2017-06-02 19:37:02 +0300

    Don't rely on the TicketPurchase model for the mimgration

--- db/migrate/20170419132148_add_week_to_ticket_purchases.rb
+++ db/migrate/20170419132148_add_week_to_ticket_purchases.rb
@@ -1,9 +1,13 @@
 class AddWeekToTicketPurchases < ActiveRecord::Migration
+  class TmpTicketPurchase < ActiveRecord::Base
+    self.table_name = 'ticket_purchases'
+  end
+
   def change
     add_column :ticket_purchases, :week, :integer
 
-    TicketPurchase.reset_column_information
-    TicketPurchase.find_each do |purchase|
+    TmpTicketPurchase.reset_column_information
+    TmpTicketPurchase.find_each do |purchase|
       purchase.week = purchase.created_at.strftime('%W')
       purchase.save!
     end
